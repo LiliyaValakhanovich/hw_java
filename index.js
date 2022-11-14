dcea22eb902e848e76cc069a07c4e913e4671dd1
Liliya <6750lilia@gmail.com>
2022-11-14 15:37:40 +0300

    updated file

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 fetch('https://dummyjson.com/products')
-  .then(res => res.json())
-  .then((data) => {
-    const products=data.products;
-    console.log(products);
+.then(res => res.json())
+.then((data) => {
+  const products=data.products;
+  console.log(products);
 
 
-    products.forEach(product => {
-      const el=document.querySelector('.wrapper');
-      el.classList.add('row-cols-1','row-cols-sm-1','row-cols-md-4','g-2');
+  products.forEach(product=>{
+    const el=document.querySelector('.wrapper');
+    el.classList.add('gy-5');
 
-      const cardCol=document.createElement('div');
-      cardCol.classList.add('col');
-      el.append(cardCol);
+    const column=document.createElement('div');
+    column.classList.add('col-md-3');
+    el.append(column);
 
-      const cardEl=document.createElement('div');
-      cardEl.classList.add('card','h-100');
-      const img=document.createElement('img');
-      img.classList.add('card-img-top');
-      img.setAttribute('src', product.thumbnail);
-      cardEl.append(img);
-      cardCol.append(cardEl);         
-      console.log(cardEl);
+    const cardEl=document.createElement('div');
+    cardEl.classList.add('card');
+    
+    const imgCont=document.createElement('div');
+    imgCont.classList.add('header');
+    const image=document.createElement('img');
+    image.classList.add('picture');
+    image.setAttribute('src', product.thumbnail);
+    imgCont.append(image);
+    cardEl.append(imgCont);
 
-      const main=document.createElement('div');
-      main.classList.add('card-body','main');
-      const wl=document.createElement('div');
-      const title=document.createElement('h5');
-      const description=document.createElement('p');
-      title.textContent=product.title;
-      description.textContent=product.description; 
-      main.append(title);
-      main.append(description);
-      main.append(wl);
-      cardEl.append(main);
+    const main=document.createElement('div');
+    main.classList.add('card-body');
+    const title=document.createElement('h5');
+    title.textContent=product.title;
+    const par=document.createElement('p');
+    par.textContent=product.description;
+    main.append(title);
+    main.append(par);
+    cardEl.append(main);
 
-      const footer=document.createElement('div');
-      footer.classList.add('card-footer');
-      const price=document.createElement('p');
-      const rating=document.createElement('p');
-      price.textContent=`Price: ${product.price}$`;
-      rating.textContent=`Rating: ${product.rating}`;
-      footer.append(price);
-      footer.append(rating);
-      main.append(footer);
+    const footer=document.createElement('div');
+    footer.classList.add('card-footer');
+    const price=document.createElement('p');
+    const rating=document.createElement('p');
+    price.textContent=product.price;
+    rating.textContent=product.rating;
+    footer.append(price);
+    footer.append(rating);
+    main.append(footer);
 
-    }); 
-        
-  });
- 
+    column.append(cardEl);
+  })
+});  
